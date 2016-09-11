6a6ba02cddeb98875f1b9e57c97a61480cb51d64
Hikaru <12kojima.takahiro@gmail.com>
2016-09-12 02:35:38 +0900

    auctex delete, helm add, haskell flycheck add

--- init.el
+++ init.el
@@ -31,6 +31,10 @@
 (unless (server-running-p)
   (server-start))
 
+;;; enable-evil
+(require 'evil)
+(evil-mode 1)
+(evil-ex-define-cmd "q[uit]" 'kill-buffer)
 ;; exec-path-from-shell
 (exec-path-from-shell-initialize)
 
@@ -103,6 +107,8 @@
 
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
 (defun my-haskell-mode-hook ()
   (interactive)
   (turn-on-haskell-indentation)
@@ -115,61 +121,16 @@
   (flymake-mode))
 (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 
-;; AUC TeX
-(auctex-latexmk-setup)
-
-(setq TeX-default-mode 'japanese-latex-mode)
- 
-(setq japanese-LaTeX-default-style "jsarticle")
-(setq TeX-output-view-style '(("^dvi$" "." "xdvi '%d'")))
-(setq preview-image-type 'dvipng)
-(add-hook 'LaTeX-mode-hook (function (lambda ()
-  (add-to-list 'TeX-command-list
-    '("pTeX" "%(PDF)ptex %`%S%(PDFout)%(mode)%' %t"
-     TeX-run-TeX nil (plain-tex-mode) :help "Run ASCII pTeX"))
-  (add-to-list 'TeX-command-list
-    '("pLaTeX" "%(PDF)platex %`%S%(PDFout)%(mode)%' %t"
-     TeX-run-TeX nil (latex-mode) :help "Run ASCII pLaTeX"))
-  (add-to-list 'TeX-command-list
-    '("evince" "evince '%s.pdf' " TeX-run-command t nil))
-  (add-to-list 'TeX-command-list
-    '("pdf" "dvipdfmx -V 4 '%s' " TeX-run-command t nil))
-)))
- 
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
- 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
- 
-(add-hook 'LaTeX-mode-hook 'visual-line-mode)
-;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-(add-hook 'TeX-mode-hook (lambda () (TeX-fold-mode 1)))
- 
-;; Change key bindings
-(add-hook 'reftex-mode-hook
- '(lambda ()
-               (define-key reftex-mode-map (kbd "\C-cr") 'reftex-reference)
-               (define-key reftex-mode-map (kbd "\C-cl") 'reftex-label)
-               (define-key reftex-mode-map (kbd "\C-cc") 'reftex-citation)
-))
- 
-;; 数式のラベル作成時にも自分でラベルを入力できるようにする
-(setq reftex-insert-label-flags '("s" "sfte"))
- 
-;; \eqrefを使う
-(setq reftex-label-alist
-      '(
-        (nil ?e nil "\\eqref{%s}" nil nil)
-        ))
- 
-; RefTeXで使用するbibファイルの位置を指定する
-;(setq reftex-default-bibliography '("~/tex/biblio.bib" "~/tex/biblio2.bib"))
-
-;;
-
 (setq twittering-icon-mode t)
 
+(require 'helm)
 (require 'helm-config)
+(helm-mode 1)
+(global-set-key "\M-x" #'helm-M-x)
+(global-set-key "\C-x\C-f" #'helm-find-files)
+(require 'helm-ag)
+(setq helm-ag-base-command "ag --nocolor --nogrou")
+(global-set-key "\C-cs" 'helm-ag)
+
+(provide 'init)
+;;; init.el ends here
